4fdd5728874ebcf38851161fbc7d073f3abe8fc6
Aluno <gabrielmelocomp@gmail.com>
2018-11-28 10:32:44 -0200

    End of class

--- game.py
+++ game.py
@@ -14,14 +14,17 @@ class Pokebola:
 class Player:
     def __init__(self, speed=2):
         self.position = [100,100]
-        self.image = pygame.image.load("resources/images/player.png")
+        self.image = pygame.image.load("resources/images/ash_alone.png")
         self.rect = None
         self.speed = speed
         self.health = Health()
 
 class Health:
     def __init__(self, count=200):
-        pass
+        self.count = count	
+        self.image = pygame.image.load("resources/images/healthbar.png")
+        self.fill = pygame.image.load("resources/images/health.png")
+        self.position = [5,5]
 
 class Pokemon:
     def __init__(self, position=[200,200], speed=4, health=4):
@@ -87,7 +90,7 @@ class App:
 
     def summon_pokemon(self):
         self.flag = False
-        self.pokemons.append(Pokemon(list([self.width, random.randint(50, self.height - 200)]), random.randint(6,10)))
+        self.pokemons.append(Pokemon(list([self.width, random.randint(50, self.height - 200)]), random.uniform(0.5,1.3)))
         time.sleep(random.randint(1,3))
         self.flag = True
 
@@ -128,22 +131,22 @@ class App:
             t = threading.Thread(target=self.summon_pokemon)
             t.start()
 
-        # TODO: Move enemy
+        # Move enemy
         for idx,pokemon in enumerate(self.pokemons):
             pokemon.position[0] -= pokemon.speed
             pokemon.rect = pygame.Rect(pokemon.image.get_rect())
             pokemon.rect.top = pokemon.position[1]
             pokemon.rect.left = pokemon.position[0]
-            # Collision between player and enemy
+
+            # Collision between ash and pokemon
             if pokemon.rect.colliderect(self.player.rect):
-                #self.player.health.count -= 2 
-                pass
+                self.player.health.count -= 1 
                             
             elif pokemon.position[0] < (0 - pokemon.rect.width):
                 self.pokemons.pop(idx)
                 #self.player.health.count -= 25
 
-            # Collision between book and enemy
+            # Collision between pokebola and pokemon
             else:
                 for idx2,pokebola in enumerate(self.pokebolas):
                     if pokemon.rect.colliderect(pokebola.rect):
@@ -160,9 +163,9 @@ class App:
         self._display_surf.blit(self.player.image, self.player.position)
         for pokemon in self.pokemons:
             self._display_surf.blit(pokemon.image, pokemon.position) 
-        #self._display_surf.blit(self.player.health.image, self.player.health.position)
-        #for i in range(self.player.health.count):
-            #self._display_surf.blit(self.player.health.fill, (self.player.health.position[0] + 3 + i, self.player.health.position[1] + 3))
+        self._display_surf.blit(self.player.health.image, self.player.health.position)
+        for i in range(self.player.health.count):
+            self._display_surf.blit(self.player.health.fill, (self.player.health.position[0] + 3 + i, self.player.health.position[1] + 3))
         for pokebola  in self.pokebolas:
             self._display_surf.blit(pokebola.image, pokebola.position)
 
